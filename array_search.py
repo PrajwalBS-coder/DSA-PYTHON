e0d0898248a2783537260d3b69fb0401d2e29124
Prajwal B S <prajwaljhon@gmail.com>
2024-10-03 22:43:16 +0530

    Array search using index

---
+++ array_search.py
@@ -0,0 +1,3 @@
+import array as myarray
+number = myarray.array('b', [2, 3, 4, 5, 6])              
+print(number.index(0))#it'll friund the index of given value if not thre it'll through error like ValueError: array.index(x): x not in array
